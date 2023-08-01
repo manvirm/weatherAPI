41cb39240013a3a15cfededde8e51c90655c58b5
Manvir Mann <manvirm29@gmail.com>
2023-07-31 22:18:15 -0700

    Check weather of city

--- main.py
+++ main.py
@@ -5,6 +5,21 @@ BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
 API_KEY = open('api_key.txt', 'r').read()
 CITY = "London"
 
+def kelvin_to_celisius_fahrenheit(kelvin):
+    celsius = kelvin - 273.15
+    fahrenheit = celsius * (9/5) + 32
+    return celsius, fahrenheit
+
 url = BASE_URL + "appid=" + API_KEY + "&q=" + CITY
 response = requests.get(url).json()
-print(response)
+temp_kelvin = response['main']['temp']
+temp_celsius, temp_fahrenheit = kelvin_to_celisius_fahrenheit(temp_kelvin)
+feels_like_kelvin = response['main']['feels_like']
+feels_like_celsius, feels_like_fahrenheit = kelvin_to_celisius_fahrenheit(feels_like_kelvin)
+wind_speed = response['wind']['speed']
+humidity = response['main']['humidity']
+description = response['weather'][0]['description']
+sunrise_time = dt.datetime.utcfromtimestamp(response['sys']['sunrise'] + response['timezone'])
+sunset_time = dt.datetime.utcfromtimestamp(response['sys']['sunset'] + response['timezone'])
+
+print(f"Temperature in {CITY}: {temp_celsius:.2f}C or {temp_fahrenheit}F")
