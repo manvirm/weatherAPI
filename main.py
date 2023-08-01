255094970d5d47bf2f6bb7624a5384b04c9da1ed
Manvir Mann <manvirm29@gmail.com>
2023-07-31 22:10:31 -0700

    Extract json data from weather map

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 import datetime as dt
 import requests
+
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+API_KEY = open('api_key.txt', 'r').read()
+CITY = "London"
+
+url = BASE_URL + "appid=" + API_KEY + "&q=" + CITY
+response = requests.get(url).json()
+print(response)
